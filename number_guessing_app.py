de18b908338937de5642428677330b37d59b5d30
Muhammad-Rubel <rubel.contact@gmail.com>
2024-03-05 17:41:18 +0600

    Refactor number guessing app code

--- number_guessing_app.py
+++ number_guessing_app.py
@@ -1,41 +1,26 @@
 import random
 import sys
 
-lower_number = int(input("Please enter the lower number "))
-upper_number = int(input("Please enter the upper number "))
+lower_num = int(input("Enter the lower number: "))
+upper_num = int(input("Enter the upper number: "))
 
-generated_number = random.randint(lower_number, upper_number)
+generated_num = random.randint(lower_num, upper_num)
 
 max_attempt = int(3)
 count = int(0)
-user_input = int(0)
 
-for num in range(lower_number, upper_number + 1):
-  user_input = int(input("Please enter a number "))
-  count += 1
+for attempt in range(max_attempt):
+  current_guess = int(input("Guess the number: "))
+  count +=1
 
-  if user_input == generated_number:
+  if current_guess == generated_num:
     print("Congratulations! You have guessed the number")
     sys.exit()
-  elif user_input < generated_number:
+  elif current_guess < generated_num:
     print("Your guess is too low")
-  elif user_input > generated_number:
+  elif current_guess > generated_num:
     print("Your guess is too high")
-
-    if(count < max_attempt):
-      break
-      
 
 
 if count == max_attempt:
-  print(f"Sorry, you have reached the maximum number of attempts. The number was {generated_number}")
-
-
-
-
-
-
-
-
-
-
+  print(f"Sorry, you have reached the maximum number of attempts. The number was {generated_num}")
